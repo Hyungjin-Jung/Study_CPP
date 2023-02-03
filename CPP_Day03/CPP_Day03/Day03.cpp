3c8662478444285d21ce0392b7d2f0569669c5f2
Hyungjin-Jung <1hyungjin1@naver.com>
2023-02-03 16:16:08 +0900

    study: 배열 선언

--- CPP_Day03/CPP_Day03/Day03.cpp
+++ CPP_Day03/CPP_Day03/Day03.cpp
@@ -1,8 +1,29 @@
 // --------- array (배열) --------------
 #include<iostream>
+#include<string>
 
 int main()
 {
+	// std::string friends[3] = {"홍길동", "성춘향", "코딩온"};	// 배열 한번에 선언
+	std::string friends[3];
+
+	friends[0] = "홍길동";
+	friends[1] = "성춘향";
+	friends[2] = "코딩온";
+
+	std::cout << friends[0] << std::endl;
+
+	double nums1[4] = { 1, 5, 2.5 };
+	nums1[3] = 9.9;
+	std::cout << nums1[3];
+
+	std::string fruit[2][3] = // 2개의 배열과 그 안에 있는 3개의 값
+	{ 
+		{"apple","grape", "lemon"},
+		{"orange","banana", "melon"}
+	};
+
+	
 
 	return 0;
 }
