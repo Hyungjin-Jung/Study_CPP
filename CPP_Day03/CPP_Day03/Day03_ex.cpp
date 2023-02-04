9ee046e61577ec06efc5529b22f14ef18462a3aa
Hyungjin-Jung <1hyungjin1@naver.com>
2023-02-05 01:35:32 +0900

    배열Fix

--- CPP_Day03/CPP_Day03/Day03_ex.cpp
+++ CPP_Day03/CPP_Day03/Day03_ex.cpp
@@ -3,24 +3,24 @@
 
 int main()
 {
-	//std::string city[5] = {"korea", "japan", "usa", "canada", "china"};
-	//for (auto c : city)
-	//{
-	//	std::cout << c << std::endl;
-	//}
-
-	int grade[5];
-	double avg = 0;
-	int i = 1;
-
-	for (int g : grade)
-	{	
-		std::cout << i << ". 학생의 성적을 입력하세요. : ";
-		std::cin >> g;
-		avg += g;
-		i++;
+	std::string city[5] = {"korea", "japan", "usa", "canada", "china"};
+	for (auto c : city)
+	{
+		std::cout << c << std::endl;
 	}
-	std::cout << "성적 평균 : " << avg / 5 << std::endl;
+
+	//double grade[5];
+	//double avg = 0;
+	//int i = 1;
+
+	//for (double g : grade)
+	//{	
+	//	std::cout << i << ". 학생의 성적을 입력하세요. : ";
+	//	std::cin >> g;
+	//	avg += g;
+	//	i++;
+	//}
+	//std::cout << "성적 평균 : " << avg / 5 << std::endl;
 
 	return 0;
 }
