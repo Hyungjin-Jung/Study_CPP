c7e626e7948c81428fb57b0c09f1c87b961333ed
Hyungjin-Jung <1hyungjin1@naver.com>
2023-02-06 18:11:02 +0900

    Day04 Study

--- Day04/Day04/Dau04.cpp
+++ Day04/Day04/Dau04.cpp
@@ -1,78 +1,181 @@
-// --------- dynamic array (동적 배열), 포인터 (pointer) --------------
+//// --------- dynamic array (동적 배열), 포인터 (pointer) --------------
+//#include<iostream>
+//#include<string>
+//
+//int main()
+//{
+//	/*
+//	int* p;
+//	int n = 5;
+//	p = &n;		// n의 주소 
+//
+//	std::cout << p << std::endl;
+//	std::cout << &n << std::endl;
+//	std::cout << n << std::endl;
+//	std::cout << *p << std::endl;
+//
+//	n = 20;
+//
+//	std::cout << p << std::endl;
+//	std::cout << *p << std::endl;
+//	
+//	*p = 15;
+//
+//	std::cout << n << std::endl;
+//	std::cout << *p << std::endl;
+//
+//	int num;
+//	std::cin >> num;
+//	// int num_array[num];	
+//	int* num_array = new int[num];	// 위 식과 같다.
+//
+//	for (int i = 0; i < num; i++)
+//	{
+//		*(num_array + i) = 1;
+//		//num_array[i] = 1;
+//	}
+//	for (int i = 0; i < num; i++)
+//	{
+//		std::cout << num_array[i] << " ";
+//	}
+//
+//	delete[] num_array;	// 동적 배열 해제(반납). 동적 메모리는 사용 후 꼭 해제하기
+//	delete[] p;
+//	*/
+//
+//	// 이중 포인터, 2차원 배열 
+//	// num을 받아서 num x num 배열
+//	// for each문은 사용할 수 없다.
+//	int num;
+//	std::cin >> num;
+//	int** num_arr = new int* [num];
+//
+//	for (int i = 0; i < num; i++)
+//	{
+//		num_arr[i] = new int[num];
+//	}
+//
+//	for (int i = 0; i < num; i++)
+//	{
+//		for (int j = 0; j < num; j++)
+//		{
+//			num_arr[i][j] = 1;
+//			std::cout << num_arr[i][j] << " ";
+//		}
+//		std::cout << std::endl;
+//	}
+//	
+//	for (int i = 0; i < num; i++)
+//	{
+//		delete[] num_arr[i];	// 안쪽 배열 해제
+//	}
+//	delete[] num_arr;	// 바깥 배열 해제
+//
+//	return 0;
+//}
+
+
+// --------------vector-------------
 #include<iostream>
-#include<string>
+#include<vector>
 
 int main()
 {
-	/*
-	int* p;
-	int n = 5;
-	p = &n;		// n의 주소 
-
-	std::cout << p << std::endl;
-	std::cout << &n << std::endl;
-	std::cout << n << std::endl;
-	std::cout << *p << std::endl;
-
-	n = 20;
-
-	std::cout << p << std::endl;
-	std::cout << *p << std::endl;
+	//std::vector<int> v;	
+	//std::vector<int> v(3);	// 크기가 3인 벡터 배열 생성
+	//std::vector<int> v(3, 1);	// 크기가 3인 벡터 배열 선언, 원소를 모두 1로 초기화
+	//std::vector<int> v = {1, 2, 3};	// 크기가 3인 벡터 배열 선언 및 초기화
 	
-	*p = 15;
+	//int num;
+	//std::cin >> num;
+	//std::vector<int> v;
+	//v.assign(num, 1);		// num의 크기, 모든 원소 1로 할당
+	////std::vector<int> v(num, 1);	// 와 같다. 
 
-	std::cout << n << std::endl;
-	std::cout << *p << std::endl;
-
-	int num;
-	std::cin >> num;
-	// int num_array[num];	
-	int* num_array = new int[num];	// 위 식과 같다.
-
-	for (int i = 0; i < num; i++)
+	std::vector<int> v = { 1, 2, 3, 4, 5 };
+	
+	for (int i = 0; i < v.size(); i++)
 	{
-		*(num_array + i) = 1;
-		//num_array[i] = 1;
+		//std::cout << v[i] << " ";		// 아래와 같다.
+		std::cout << v.at(i) << " ";
 	}
-	for (int i = 0; i < num; i++)
-	{
-		std::cout << num_array[i] << " ";
+	std::cout << std::endl;
+	
+	v.resize(6);	// 배열의 크기를 resize, 커진 길이만큼 0으로 초기화
+	v.push_back(5);	// 배열의 마지막에 원하는 원소를 추가함.
+	for (int i = 0; i < v.size(); i++)
+	{	
+		std::cout << v.at(i) << " ";	// at은 index로 접근하기 휘함.
 	}
+	std::cout << std::endl;
 
-	delete[] num_array;	// 동적 배열 해제(반납). 동적 메모리는 사용 후 꼭 해제하기
-	delete[] p;
-	*/
-
-	// 이중 포인터, 2차원 배열 
-	// num을 받아서 num x num 배열
-	// for each문은 사용할 수 없다.
-	int num;
-	std::cin >> num;
-	int** num_arr = new int* [num];
-
-	for (int i = 0; i < num; i++)
+	v.pop_back();	// 마지막 원소를 지운다. 
+	for (int i = 0; i < v.size(); i++)
 	{
-		num_arr[i] = new int[num];
+		std::cout << v.at(i) << " ";
 	}
+	std::cout << std::endl;
+	std::cout << v.front() << " " << v.back() << std::endl;	// V.front(): 가장 처음 원소 출력, v.back(): 가장 마지막 원소 출력
 
-	for (int i = 0; i < num; i++)
+	v.insert(v.begin() + 2, 99);	// 2번 index 뒤에 99를 추가함
+	for (int i = 0; i < v.size(); i++)
 	{
-		for (int j = 0; j < num; j++)
+		std::cout << v.at(i) << " ";
+	}
+	std::cout << std::endl;
+
+	v.erase(v.begin() + 3);	// 3번 index를 삭제
+	for (int i = 0; i < v.size(); i++)
+	{
+		std::cout << v.at(i) << " ";
+	}
+	std::cout << std::endl;
+
+	v.erase(v.begin() + 3, v.begin() + 5);	// 3번부터 5번(5번 제외) index를 삭제
+	for (int i = 0; i < v.size(); i++)
+	{
+		std::cout << v.at(i) << " ";
+	}
+	std::cout << std::endl;
+
+	//v.clear();		// 배열의 모든 원소 삭제, size = 0
+	//for (int i = 0; i < v.size(); i++)
+	//{
+	//	std::cout << v.at(i) << " ";
+	//}
+	//std::cout << std::endl;
+
+	for (int &vv : v)
+	{
+		vv = vv + 1;
+		std::cout << vv << " ";
+	}
+	std::cout << std::endl;
+	
+	// -----------------2차원 벡터 --------------
+	std::vector<std::vector<int>> v2;
+	v2.assign(3, std::vector<int>(2,1));	// 2x3 행열 생성
+
+	for (int i = 0; i < v2.size(); i++)	// v2 size는 행의 크기
+	{
+		for (int j = 0; j < v2.at(i).size(); j++)
 		{
-			num_arr[i][j] = 1;
-			std::cout << num_arr[i][j] << " ";
+			std::cout << v2.at(i).at(j) << " ";
+			// std::cout << v2[i][j] << " ";	// 위랑 같다.
 		}
 		std::cout << std::endl;
 	}
-	
-	for (int i = 0; i < num; i++)
+
+	for (std::vector<int> vv2 : v2)
 	{
-		delete[] num_arr[i];	// 안쪽 배열 해제
+		for (int vvv2 : vv2)
+		{
+			std::cout << vvv2 << " ";
+		}
+		std::cout << std::endl;
 	}
-	delete[] num_arr;	// 바깥 배열 해제
-
-	// vector
-
 
 	return 0;
 }
+
+
--- Day04/Day04/Day04_Ex.cpp
+++ Day04/Day04/Day04_Ex.cpp
@@ -1,78 +1,217 @@
-// --------- dynamic array (동적 배열), 포인터 (pointer) --------------
-#include<iostream>
+//  동적 배열, 포인터 실습 1
+
+/*#include<iostream>
 #include<string>
 
-int main()
+void dy_arr(int x, int y)
 {
-	/*
-	int* p;
-	int n = 5;
-	p = &n;		// n의 주소
-
-	std::cout << p << std::endl;
-	std::cout << &n << std::endl;
-	std::cout << n << std::endl;
-	std::cout << *p << std::endl;
-
-	n = 20;
-
-	std::cout << p << std::endl;
-	std::cout << *p << std::endl;
-
-	*p = 15;
-
-	std::cout << n << std::endl;
-	std::cout << *p << std::endl;
-
-	int num;
-	std::cin >> num;
-	// int num_array[num];
-	int* num_array = new int[num];	// 위 식과 같다.
-
-	for (int i = 0; i < num; i++)
+	int n1 = 0;
+	int** dy_arr = new int* [x];
+	for (int i = 0; i < x; i++)
 	{
-		*(num_array + i) = 1;
-		//num_array[i] = 1;
-	}
-	for (int i = 0; i < num; i++)
-	{
-		std::cout << num_array[i] << " ";
+		dy_arr[i] = new int[y];
 	}
 
-	delete[] num_array;	// 동적 배열 해제(반납). 동적 메모리는 사용 후 꼭 해제하기
-	delete[] p;
-	*/
-
-	// 이중 포인터, 2차원 배열 
-	// num을 받아서 num x num 배열
-	// for each문은 사용할 수 없다.
-	int num;
-	std::cin >> num;
-	int** num_arr = new int* [num];
-
-	for (int i = 0; i < num; i++)
+	for (int i = 0; i < x; i++)
 	{
-		num_arr[i] = new int[num]; 
-	}
-
-	for (int i = 0; i < num; i++)
-	{
-		for (int j = 0; j < num; j++)
+		for (int j = 0; j < y; j++)
 		{
-			num_arr[i][j] = 1;
-			std::cout << num_arr[i][j] << " ";
+			n1 += 1;
+			dy_arr[i][j] = n1;
+			std::cout << dy_arr[i][j] << " ";
 		}
 		std::cout << std::endl;
 	}
 
-	for (int i = 0; i < num; i++)
+	for (int i = 0; i < x; i++)
 	{
-		delete[] num_arr[i];	// 안쪽 배열 해제
+		delete[] dy_arr[i];	// 안쪽 배열 해제
 	}
-	delete[] num_arr;	// 바깥 배열 해제
+	delete[] dy_arr;	// 바깥 배열 해제
+}
 
-	// vector
-
+int main()
+{
+	int x, y;
+	std::cout << "x를 입력하세요. : ";
+	std::cin >> x;
+	std::cout << "y를 입력하세요. : ";
+	std::cin >> y;
+	if (x > 0 && y > 0)
+	{
+		dy_arr(x, y);
+	}
+	else
+	{
+		std::cout << "x와 y 모두 양수를 입력해주세요. " << std::endl;
+		std::cout << "x를 입력하세요. : ";
+		std::cin >> x;
+		std::cout << "y를 입력하세요. : ";
+		std::cin >> y;
+		dy_arr(x, y);
+	}
 
 	return 0;
 }
+*/
+// --------- 선생님 풀이 -----------
+//#include<iostream>
+//#include<string>
+//
+//void dy_arr(int x, int y)
+//{
+//	int** dy_arr = new int* [x];
+//	int n1 = 1;
+//
+//	for (int i = 0; i < x; i++)
+//	{
+//		dy_arr[i] = new int[y];
+//	}
+//
+//	for (int i = 0; i < x; i++)
+//	{
+//		for (int j = 0; j < y; j++)
+//		{
+//			dy_arr[i][j] = n1++;
+//			std::cout << dy_arr[i][j] << " ";
+//		}
+//		std::cout << std::endl;
+//	}
+//
+//	for (int i = 0; i < x; i++)
+//	{
+//		delete[] dy_arr[i];	// 안쪽 배열 해제
+//	}
+//	delete[] dy_arr;	// 바깥 배열 해제
+//}
+//
+//int main()
+//{
+//	int x, y;
+//	
+//	while (1)
+//	{
+//		std::cout << "x를 입력하세요. : ";
+//		std::cin >> x;
+//		std::cout << "y를 입력하세요. : ";
+//		std::cin >> y;
+//		if (x > 0 && y > 0)
+//			break;
+//		else 
+//			std::cout << "x와 y 모두 양수를 입력해주세요. " << std::endl;
+//	}
+//
+//	dy_arr(x, y);
+//
+//	return 0;
+//}
+
+//[번외] 실습.배열 실습2 응용!
+//*사진은 참고용입니다~.아래 글을 읽고 문제 해결해주세요!
+//(1) 몇명의 학생 성적 평균을 구할 지 입력 받기
+//(2) 입력받은 학생 수 만큼 성적을 입력받기
+//(3) 학생들의 성적 평균 산출하기
+//input
+//학생의 수를 입력하세요 : 3
+//1번째 학생의 성적을 입력하세요 : 10
+//2번째 학생의 성적을 입력하세요 : 20
+//3번째 학생의 성적을 입력하세요 : 30
+//output
+//평균 : ? ? ? ?
+
+//#include<iostream>
+//#include<string>
+//
+//int main()
+//{
+//	int num = 0; 
+//	double total = 0;
+//	std::cout << "학생의 수를 입력하세요. : ";
+//	std::cin >> num;
+//	double* score = new double[num];
+//
+//	for (int i = 0; i < num; i++)
+//	{
+//		std::cout << i + 1 <<"번째 학생의 성적을 입력하세요. : ";
+//		std::cin >> score[i];
+//		total += score[i];
+//	}
+//
+//	std::cout << "평균 점수는 : " << total / num << std::endl;
+//
+//	return 0;
+//}
+
+// vector 실습 2
+
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//
+//void dy_arr(int x, int y)
+//{
+//	std::vector<std::vector<int>> dy_arr;
+//	dy_arr.assign(x, std::vector<int>(y, 1));
+//	int n1 = 1;
+//
+//	for (int i = 0; i < x; i++)
+//	{
+//		for (int j = 0; j < y; j++)
+//		{
+//			dy_arr.at(i).at(j) = n1++;
+//			std::cout << dy_arr.at(i).at(j) << " ";
+//		}
+//		std::cout << std::endl;
+//	}
+//}
+//
+//int main()
+//{
+//	int x, y;
+//	
+//	while (1)
+//	{
+//		std::cout << "x를 입력하세요. : ";
+//		std::cin >> x;
+//		std::cout << "y를 입력하세요. : ";
+//		std::cin >> y;
+//		if (x > 0 && y > 0)
+//			break;
+//		else 
+//			std::cout << "x와 y 모두 양수를 입력해주세요. " << std::endl;
+//	}
+//
+//	dy_arr(x, y);
+//
+//	return 0;
+//}
+
+// vector 실습 2
+// (1) 사용자가 입력한 숫자들을 배열에 담고, 그 수의 합 구하기.
+// (2) 출력형태는 사진과 같음.
+
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//
+//int main()
+//{
+//	int num, sum = 0;
+//	std::vector<int> v;
+//
+//	std::cout << "사용자가 입력한 숫자 더하기. " << std::endl;
+//
+//	do
+//	{
+//		int i = 0;
+//		std::cout << "숫자를 입력하세요. (0: exit) : " ;
+//		std::cin >> num;
+//		v.insert(v.begin(), num);
+//		sum += v.at(i);
+//		i++;
+//	} while (num);
+//
+//	std::cout << "사용자가 입력한 수의 합은 : " << sum << std::endl;
+//
+//	return 0;
+//}
