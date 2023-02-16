2b5f2e7b61e282909217a6c636b359ee08e8796c
Hyungjin-Jung <1hyungjin1@naver.com>
2023-02-16 16:07:12 +0900

    끝말잇기

--- Day11/Day11/Time_attack.cpp
+++ Day11/Day11/Time_attack.cpp
@@ -10,13 +10,44 @@ int main()
 {
 	clock_t startTime = clock();
 	// ~~~~~~~ ( 타임어택 코드 )
-	clock_t endTime = clock();
-	cout << "걸린 시간 : " << (endTime - startTime) / CLOCKS_PER_SEC << endl;
 
-	vector<string> word = {};
-	word.at(0) = "airplane";
-	cout << word.at(0) << endl;
+	string input, output = "airplane";
+	int n, timer = 0, count = 0, count_check;
 
+	while (1)
+	{
+		n = output.size();
+		cout << output << endl;
+
+		cout << "다음 단어를 입력하세요. : ";
+		cin >> input;
+
+		if (input[0] == output[n - 1])
+		{
+			output += " -> " + input;
+			count++;
+			//cout << endl;
+		}
+		else
+		{
+			cout << "잘못된 입력입니다. \n";
+		}
+
+		clock_t endTime = clock();
+		timer = (endTime - startTime) / CLOCKS_PER_SEC;
+
+		cout << "경과한 시간: " << timer << endl << endl;
+
+		if (timer > 30)
+		{
+			if (count_check != count)
+			count--;
+
+			cout << "게임 종료 \n" << "총 입력한 단어 개수 : " << count;
+			break;
+		}
+		count_check = count;
+	}
 
 	return 0;
 }
