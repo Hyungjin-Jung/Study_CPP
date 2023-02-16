cd3c7e031d19efbde7b731723d76c6d8383b6604
Hyungjin-Jung <1hyungjin1@naver.com>
2023-02-16 14:14:40 +0900

    베스킨라빈스31 게임 수정

--- Day11/Day11/Day11.cpp
+++ Day11/Day11/Day11.cpp
@@ -5,58 +5,58 @@
 
 using namespace std;
 
+void baskin(int count, int num)
+{
+
+}
 // 배스킨라빈스 31 게임
 int main()
 {
-	int num = 0, count = 1;
+	int num = 0, count = 1, k = 0;
 	bool turn = false;
 
-	while (1)
+	while (count < 32)
 	{
-		cout << "개수를 입력하세요. : ";
-		cin >> num;
-
-		if (num < 4 && num > 0)
+		if (turn == true)
 		{
-			cout << "사용자가 부른 숫자!! \n";
-			for (int i = 0; i < num; i++)
-			{
-				cout << count + i << endl;
-			}
+			std::srand(time(NULL));
+			int com = std::rand() % 3 + 1;
 
-			count += num;
-			turn = true;
-
-			if (count > 31)
-			{
-				cout << "게임 종료!! 컴퓨터의 승리입니다. \n";
-				break;
-			}
+			cout << "컴퓨터가 부른 숫자!! \n";
+			num = com;
+			turn = false;
 		}
 		else
 		{
-			cout << "1 ~ 3의 숫자를 입력해주세요. \n";
+			cout << "개수를 입력하세요. : ";
+			cin >> num;
+
+			if (num > 3 || num < 1)
+			{
+				cout << "1 ~ 3의 숫자를 입력해주세요. \n";
+				num = 0;
+				turn = false;
+			}
+			else
+			{
+				cout << "사용자가 부른 숫자!! \n";
+				turn = true;
+			}
 		}
-
-		std::srand(time(NULL));
-		int com = std::rand() % 3 + 1;
-
-		if (turn == true)
+		for (int i = 0; i < num; i++)
 		{
-			cout << "컴퓨터가 부른 숫자!! \n";
-			for (int i = 0; i < com; i++)
-			{
-				cout << count + i << endl;
-			}
-			count += com;
-			turn = false;
-			if (count > 31)
-			{
-				cout << "게임 종료!! 사용자의 승리입니다. \n";
-				break;
-			}
+			cout << count + i << endl;
 		}
+		count += num;
+		k++;
 	}
+
+	if (turn == true)
+	{
+		cout << "게임 종료!! 컴퓨터의 승리입니다. \n";
+	}
+	else
+		cout << "게임 종료!! 사용자의 승리입니다. \n";
 
 	return 0;
 }
