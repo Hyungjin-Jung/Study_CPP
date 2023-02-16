2e488ea4f235e50da10244346bc78109d9cd4e15
Hyungjin-Jung <1hyungjin1@naver.com>
2023-02-16 16:38:01 +0900

    끝말잇기 해치움

--- Day11/Day11/Time_attack.cpp
+++ Day11/Day11/Time_attack.cpp
@@ -77,28 +77,25 @@ int main()
 		cout << "다음 단어를 입력하세요. : ";
 		cin >> input;
 
-		if (input[0] == output[n - 1])
+		clock_t endTime = clock();
+		timer = (endTime - startTime) / CLOCKS_PER_SEC;
+
+		if (timer > 10)
+		{
+			cout << endl <<"게임 종료 \n" << "총 입력한 단어 개수 : " << count;
+			break;
+		}
+		else if (input[0] == output[n - 1])
 		{
 			output += " -> " + input;
+			count++;
 			//cout << endl;
 		}
 		else
 		{
 			cout << "잘못된 입력입니다. \n";
-			count--;
 		}
-
-		clock_t endTime = clock();
-		timer = (endTime - startTime) / CLOCKS_PER_SEC;
-
 		cout << "경과한 시간: " << timer << endl << endl;
-
-		if (timer > 30)
-		{
-			cout << "게임 종료 \n" << "총 입력한 단어 개수 : " << count;
-			break;
-		}
-		count++;
 	}
 
 	return 0;
