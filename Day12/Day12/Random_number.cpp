61d4eb399bc2e5b4d5ba83296eaaa5a6aa9f25dc
Hyungjin-Jung <1hyungjin1@naver.com>
2023-02-16 17:23:41 +0900

    랜덤숫자 맞추기

---
+++ Day12/Day12/Random_number.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+// 랜덤 숫자 맞추기
+int main()
+{
+	std::srand(time(NULL));
+
+	int lotto[6] = {};
+	int user[6] = {};
+	int count = 0, user_num = 0;
+
+	while (count < 6)
+	{
+		bool dupli = false;
+		int num = std::rand() % 25 + 1;
+		for (int i = 0; i < 6; i++)
+		{
+			if (lotto[i] == num)
+			{
+				dupli = true;
+				break;
+			}
+		}
+		if (!dupli)
+		{
+			lotto[count] = num;
+			count++;
+		}
+	}
+	cout << "로또 번호는 : ";
+	for (int i = 0; i < 6; i++)
+	{
+		cout << lotto[i] << " ";
+	}
+	cout << endl;
+
+	count = 0;
+	while (count < 6)
+	{
+		bool dupli = false;
+		cout << count + 1 << "번째 번호를 입력하세요. : ";
+		cin >> user_num;
+		if (user_num > 25 || user_num < 1)
+		{
+			cout << "잘못된 숫자입니다. 다시 입력해주세요. \n";
+		}
+		else
+		{
+			for (int i = 0; i < 6; i++)
+			{
+				if (user[i] == user_num)
+				{
+					dupli = true;
+					cout << "이미 입력된 숫자입니다.\n";
+					break;
+				}
+			}
+			if (!dupli)
+			{
+				user[count] = user_num;
+				count++;
+			}
+		}
+
+	}
+
+	cout << "나의 번호 : ";
+
+	for (int i = 0; i < 6; i++)
+	{
+		cout << user[i] << " ";
+	}
+
+	count = 0;
+
+	for (int j = 0; j < 6; j++)
+	{
+		lotto[j];
+		for (int i = 0; i < 6; i++)
+		{
+			if (lotto[j] == user[i])
+			{
+				count++;
+			}
+		}
+	}
+	cout << endl << "맞춘 개수: " << count << endl;
+
+	cout << 7 - count << "등!! " << endl;
+
+	return 0;
+}
