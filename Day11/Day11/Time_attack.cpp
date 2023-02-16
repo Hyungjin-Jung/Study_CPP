13ebe1ab4296c8854d39a4c79f8dc5525c5445e9
Hyungjin-Jung <1hyungjin1@naver.com>
2023-02-16 16:24:55 +0900

    끝말잇기 최종?

--- Day11/Day11/Time_attack.cpp
+++ Day11/Day11/Time_attack.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
@@ -38,7 +39,7 @@ int main()
 
 		cout << "경과한 시간: " << timer << endl << endl;
 
-		if (timer > 30)
+		if (timer > 10)
 		{
 			if (count_check != count)
 			count--;
@@ -47,6 +48,57 @@ int main()
 			break;
 		}
 		count_check = count;
+	}
+
+	return 0;
+}
+*/
+
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<vector>
+
+using namespace std;
+
+// 타임어택 끝말잇기 게임
+int main()
+{
+	clock_t startTime = clock();
+
+	string input, output = "airplane";
+	int n, timer = 0, count = 0;
+
+	while (1)
+	{
+		n = output.size();
+		cout << output << endl;
+
+		cout << "다음 단어를 입력하세요. : ";
+		cin >> input;
+
+		if (input[0] == output[n - 1])
+		{
+			output += " -> " + input;
+			//cout << endl;
+		}
+		else
+		{
+			cout << "잘못된 입력입니다. \n";
+			count--;
+		}
+
+		clock_t endTime = clock();
+		timer = (endTime - startTime) / CLOCKS_PER_SEC;
+
+		cout << "경과한 시간: " << timer << endl << endl;
+
+		if (timer > 30)
+		{
+			cout << "게임 종료 \n" << "총 입력한 단어 개수 : " << count;
+			break;
+		}
+		count++;
 	}
 
 	return 0;
