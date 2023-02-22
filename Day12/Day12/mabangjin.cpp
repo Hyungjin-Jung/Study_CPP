4ab67c03458b2481c226a383c199360d24d52c65
Hyungjin-Jung <1hyungjin1@naver.com>
2023-02-23 00:50:19 +0900

    마방진 수정

--- Day12/Day12/mabangjin.cpp
+++ Day12/Day12/mabangjin.cpp
@@ -2,9 +2,11 @@
 #include<iostream>
 #include<string>
 
+using namespace std;
+
 int main()
 {
-	int num;
+	int num, k = 0;
 	std::cin >> num;
 	int** num_arr = new int* [num];		// 행의 크기가 num인 이차원 배열
 
@@ -16,11 +18,16 @@ int main()
 	{
 		for (int j = 0; j < num; j++)
 		{
-			num_arr[i][j] = 1;
+			k++;
+			num_arr[i][j] = k;
+			//std::cout << num_arr[i][j] << " ";
 			std::cout << num_arr[i][j] << " ";
 		}
+		//std::cout << std::endl;
 		std::cout << std::endl;
 	}
+
+
 
 	for (int i = 0; i < num; i++)
 	{
@@ -31,3 +38,66 @@ int main()
 	return 0;
 }
 */
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int num = 0;
+	cin >> num;
+	int** num_arr = new int* [num];		// 행의 크기가 num인 이차원 배열
+	int center = (num + 1) / 2;
+	int value = 1;
+
+	int i = 1;
+	int j = center;
+
+	for (int k = 0; k < num * num; k++)
+	{// 각각의 index에 선언하고자 하는 배열의 크기만큼을 가리키게 한다.
+		num_arr[k] = new int[num * num];		// 각각의 행에 크기가 num인 배열을 할당
+	}
+
+	for (int k = 0; k < num * num; k++) 
+	{
+		num_arr[i][j] = value;
+		if (value % num == 0) 
+		{ 
+			i++; 
+		}
+		else 
+		{
+			i--;
+			j++;
+		}
+		if (i < 1) 
+		{ 
+			i = num; 
+		}
+		if (j > num) 
+		{ 
+			j = 1; 
+		}
+		value++;
+
+	}
+
+	for (int i = 1; i <= num; i++) 
+	{
+		for (int j = 1; j <= num; j++) 
+		{
+			cout << num_arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int k = 0; k < num * num; k++)
+	{
+		delete[] num_arr[k];	// 안쪽 배열 해제
+	}
+	delete[] num_arr;	// 바깥 배열 해제
+
+	return 0;
+}
