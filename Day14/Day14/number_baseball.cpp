46288e5edf39924538217ec1c595156b6f6bdf3c
Hyungjin-Jung <1hyungjin1@naver.com>
2023-02-23 17:16:27 +0900

    숫자 야구 게암 최종?

--- Day14/Day14/number_baseball.cpp
+++ Day14/Day14/number_baseball.cpp
@@ -12,8 +12,7 @@ int main()
 	const int amount = 3;
 	int com[amount] = {};
 	int user[amount] = {};
-	int count = 0, user_num = 0; 
-	int value_count = 0, place_count = 0, num_count = 0;
+	int count = 0, value_count = 0, place_count = 0, user_num = 0;
 	bool dupli = false;
 
 	while (count < amount)
@@ -24,39 +23,39 @@ int main()
 		{
 			if (com[i] == num)
 			{
-				dupli = true;
+				dupli = true;	// 중복되는 숫자시 저장하지 않고 탈출
 				break;
 			}
 		}
 		if (!dupli)
 		{
-			com[count] = num;
-			count++;
+			com[count] = num;	// 숫자 저장
+			count++;			// 몇개 저장했는지 카운트
 		}
 	}
 
+	/*
 	cout << "컴퓨터의 번호 : ";
-
 	for (int i = 0; i < amount; i++)
 	{
-		cout << com[i] << " ";
+		cout << com[i] << " ";	// 컴퓨터 번호 출력
 	}
 	cout << endl << endl;
-
+	*/
 	count = 0;
 
 	do
 	{
+		count++;
 		cout << "1 ~ 9 사이의 숫자 3개를 입력 하시오. (이외의 숫자: 종료)" << endl << "입력한 수: ";
-		num_count++;
-		while (count < amount)
+		
+		for (int i = 0; i < amount; i++)
 		{
 			cin >> user_num;
-			user[count] = user_num;
-			count++;
+			user[i] = user_num;	// 입력 받은 수를 배열에 저장
 			if (user_num > 9 || user_num < 1)
 			{
-				dupli = true;
+				dupli = true;	// 1 ~ 9 범위를 벗어나면 종료
 			}
 		}
 
@@ -64,13 +63,13 @@ int main()
 		{
 			if (com[j] == user[j])
 			{
-				place_count++;
+				place_count++;	// 자리와 숫자가 같을 때 카운트 (Ball)
 			}
 			for (int i = 0; i < amount; i++)
 			{
 				if (com[j] != user[j] && com[i] == user[j])
 				{
-					value_count++;
+					value_count++;	// 자리는 다르고 숫자만 같을 때 카운트 (Strike)
 				}
 			}
 		}
@@ -79,12 +78,11 @@ int main()
 
 		if (place_count == 3)
 		{
-			cout << num_count << "번 만에 맞췄습니다." << endl;
+			cout << count << "번 만에 맞췄습니다." << endl;
 			break;
 		}
 		else
 		{
-			count = 0;
 			place_count = 0;
 			value_count = 0;
 		}
@@ -92,103 +90,3 @@ int main()
 
 	return 0;
 }
-
-/*
-#include<iostream>
-#include<cstdlib>
-#include<ctime>
-
-using namespace std;
-
-// 숫자 야구 게임
-int main()
-{
-	std::srand(time(NULL));
-
-	int com[3] = {};
-	int user[3] = {};
-	int count = 0, user_num = 0, amount = 3;
-
-	while (count < amount)
-	{
-		bool dupli = false;
-		int num = std::rand() % 9 + 1;
-		for (int i = 0; i < amount; i++)
-		{
-			if (com[i] == num)
-			{
-				dupli = true;
-				break;
-			}
-		}
-		if (!dupli)
-		{
-			com[count] = num;
-			count++;
-		}
-	}
-
-	count = 0;
-
-	while (count < amount)
-	{
-		bool dupli = false;
-		cout << count + 1 << "번째 번호를 입력하세요. : ";
-		cin >> user_num;
-		if (user_num > 9 || user_num < 1)
-		{
-			cout << "잘못된 숫자입니다. 다시 입력해주세요. \n";
-		}
-		else
-		{
-			for (int i = 0; i < amount; i++)
-			{
-				if (user[i] == user_num)
-				{
-					dupli = true;
-					cout << "이미 입력된 숫자입니다.\n";
-					break;
-				}
-			}
-			if (!dupli)
-			{
-				user[count] = user_num;
-				count++;
-			}
-		}
-	}
-	cout << "-----------------------------------\n";
-
-	cout << endl << "컴퓨터의 번호 : ";
-	for (int i = 0; i < amount; i++)
-	{
-		cout << com[i] << " ";
-	}
-
-	cout << endl << "나의 번호 : ";
-	for (int i = 0; i < amount; i++)
-	{
-		cout << user[i] << " ";
-	}
-
-	count = 0;
-
-	for (int j = 0; j < amount; j++)
-	{
-		com[j];
-		for (int i = 0; i < amount; i++)
-		{
-			if (com[j] == user[i])
-			{
-				count++;
-			}
-		}
-	}
-
-	cout << endl << "맞춘 개수에 따라 1 ~ 7등까지의 결과가 나옵니다. " << endl;
-	cout << endl << "맞춘 개수: " << count << endl;
-	cout << 7 - count << "등!! " << endl;
-
-	return 0;
-}
-*/
