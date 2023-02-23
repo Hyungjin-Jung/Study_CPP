8519aaf14cef9c61ff422c27457d1c14923d04ab
Hyungjin-Jung <1hyungjin1@naver.com>
2023-02-23 15:16:51 +0900

    마방진 해치웠다..

--- Day12/Day12/mabangjin.cpp
+++ Day12/Day12/mabangjin.cpp
@@ -6,72 +6,137 @@ using namespace std;
 int main()
 {
 	int num = 0;
-	//cin >> num;
+
+	while (1) 
+	{
+		cout << "마방진의 행 혹은 열의 수를 자연수로 입력해주세요 : ";
+		cin >> num;
+		cout << endl;
+		if (num > 0) 
+			break;
+	}
+
+	int** num_arr = new int* [num];		// 행의 크기가 num인 이차원 배열
+	int center = num / 2;
+	int i = 0;
+	int j = center;
+
+	for (int k = 0; k < num; k++)
+	{// 각각의 index에 선언하고자 하는 배열의 크기만큼을 가리키게 한다.
+		num_arr[k] = new int[num];		// 각각의 행에 크기가 num인 배열을 할당
+	}
+
+	for (int k = 1; k <= num * num; k++)
+	{
+		num_arr[i][j] = k;
+		if (k % num == 0)
+		{
+			i++;
+		}
+		else
+		{
+			i--;
+			j++;
+		}
+		if (i < 0)
+		{
+			i = num - 1;
+		}
+		if (j >= num)
+		{
+			j = 0;
+		}
+	}
+
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = 0; j < num; j++)
+		{
+			cout << num_arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int k = 0; k < num; k++)
+	{
+		delete[] num_arr[k];	// 안쪽 배열 해제
+	}
+	delete[] num_arr;	// 바깥 배열 해제
+
+	return 0;
+}
+
+/*
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	int num = 0;
 
 	while (1)
 	{
 		cout << "마방진의 행 혹은 열의 수를 자연수로 입력해주세요 : ";
 		cin >> num;
 		cout << endl;
-
 		if (num > 0)
-		{
-			int** num_arr = new int* [num];		// 행의 크기가 num인 이차원 배열
-			int center = num / 2;
-			int value = 1;
-
-			int i = 0;
-			int j = center;
-
-			for (int k = 0; k < num; k++)
-			{// 각각의 index에 선언하고자 하는 배열의 크기만큼을 가리키게 한다.
-				num_arr[k] = new int[num];		// 각각의 행에 크기가 num인 배열을 할당
-			}
-
-			for (int k = 0; k < num * num; k++)
-			{
-				num_arr[i][j] = value;
-				if (value % num == 0)
-				{
-					i++;
-				}
-				else
-				{
-					i--;
-					j++;
-				}
-				if (i < 0)
-				{
-					i = num - 1;
-				}
-				if (j >= num)
-				{
-					j = 0;
-				}
-				value++;
-			}
-
-			for (int i = 0; i < num; i++)
-			{
-				for (int j = 0; j < num; j++)
-				{
-					cout << num_arr[i][j] << " ";
-				}
-				cout << endl;
-			}
-
-			for (int k = 0; k < num; k++)
-			{
-				delete[] num_arr[k];	// 안쪽 배열 해제
-			}
-			delete[] num_arr;	// 바깥 배열 해제
-
 			break;
-		}
 	}
+	int** num_arr = new int* [num];		// 행의 크기가 num인 이차원 배열
+	int center = num / 2;
+	int value = 1;
+
+	int i = 0;
+	int j = center;
+
+	for (int k = 0; k < num; k++)
+	{// 각각의 index에 선언하고자 하는 배열의 크기만큼을 가리키게 한다.
+		num_arr[k] = new int[num];		// 각각의 행에 크기가 num인 배열을 할당
+	}
+
+	for (int k = 0; k < num * num; k++)
+	{
+		num_arr[i][j] = value;
+		if (value % num == 0)
+		{
+			i++;
+		}
+		else
+		{
+			i--;
+			j++;
+		}
+		if (i < 0)
+		{
+			i = num - 1;
+		}
+		if (j >= num)
+		{
+			j = 0;
+		}
+		value++;
+	}
+
+	for (int i = 0; i < num; i++)
+	{
+		for (int j = 0; j < num; j++)
+		{
+			cout << num_arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int k = 0; k < num; k++)
+	{
+		delete[] num_arr[k];	// 안쪽 배열 해제
+	}
+	delete[] num_arr;	// 바깥 배열 해제
 
 	return 0;
 }
+*/
 /*
 #include<iostream>
 #include<string>
